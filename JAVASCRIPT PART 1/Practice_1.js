bc4451d40a80de8b8d4d71b185821ac68feb7a62
PranjalGits <pranjalthakre02@gmail.com>
2024-03-23 12:07:46 +0530

    first commit

---
+++ JAVASCRIPT PART 1/Practice_1.js
@@ -0,0 +1,24 @@
+// let age = 23;
+// age = age + 2;
+// console.log(age)
+
+
+// let hindi = 80;
+// let eng = 90;
+// let math = 100;
+// let avg = (hindi + math + eng)/3;
+// console.log(avg)
+
+
+// let num = 5;
+// // let newNum = num++;
+// // console.log(newNum)
+
+
+// let num = 5;
+// let newNum = ++num;
+// console.log(newNum)
+
+let isAdult = true;
+console.log(isAdult)
+console.log(typeof isAdult)
---
+++ JAVASCRIPT PART 1/javascript.js
@@ -0,0 +1,46 @@
+// console.log(1+2)
+
+// a=10
+// b=20
+// console.log(a+b)
+
+// c=30
+// Name="Pranjal"
+// console.log(typeof c)
+// console.log(typeof Name)
+// console.log(typeof "Thakre")
+
+// console.log(0/0)
+// console.log(NaN+1)
+
+// console.log((5+2)/7+1*2)
+// console.log((2+1)*3)
+// console.log(3/1+2**2)
+// console.log(4+1*6/2)
+
+
+// let num1 = 20;
+// let num2 = 30;
+// let sum = num1+num2;
+// console.log(sum)
+// let side = 10;
+// let area = side * side;
+// console.log(area)
+
+
+// let num =3;
+// console.log(num)
+// num=5
+// console.log(num)
+
+
+// const year = 2026;
+// console.log(year)
+// year = 2025;        // Error
+// console.log(year)
+
+
+const pi = 3.14;
+let radius = 4;
+let area = pi * radius**2;
+console.log(area)
