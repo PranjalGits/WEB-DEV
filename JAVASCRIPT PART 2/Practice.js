0cb2d92185315484ca8fb9cec6a5403ccf02870e
PranjalGits <pranjalthakre02@gmail.com>
2024-03-27 13:16:57 +0530

    first commit

---
+++ JAVASCRIPT PART 2/Practice.js
@@ -0,0 +1,38 @@
+// let color1 = "red";
+// let color2 = "green";
+// let color3 = "yellow";
+
+// if (color1 = "red") {
+//   console.log("Stop your Vehicle");
+// }
+
+// if (color2 = "green") {
+//   console.log("Go your Vehicle");
+// }
+
+// if (color3 = "yellow") {
+//   console.log("SlowDown your Vehicle");
+// }
+
+
+let size = "p";
+
+if (size == "XL") {
+  console.log("Popcorn Prices is Rs.250");
+}
+
+else if (size == "L") {
+  console.log("Popcorn Prices is Rs.200");
+}
+
+else if (size == "M") {
+  console.log("Popcorn Prices is Rs.100");
+}
+
+else if (size == "S") {
+  console.log("Popcorn Prices is Rs.50");
+}
+
+else {
+  console.log("Tum Garib Hooooo......!!!!!")
+}
---
+++ JAVASCRIPT PART 2/app.js
@@ -0,0 +1,7 @@
+// console.log("Hello World");
+
+let a = 23;
+if(a>=18) {
+  console.log("You Can Vote");
+  console.log("You can Drive");
+}
