29dc0430b609fdabc851cb4be8aed2eb2123738f
PranjalGits <pranjalthakre02@gmail.com>
2024-03-28 11:18:41 +0530

    first commit

---
+++ JAVASCRIPT PART 3/Practice.js
@@ -0,0 +1,12 @@
+// let msg = "    help!     ";
+// let finalMsg = msg.toUpperCase().trim()
+// console.log(finalMsg)
+
+
+// let Name = "ApnaCollege";
+// let Name_slice = Name.slice(4,9);
+// let Name_index = Name.indexOf("na");
+// let Name_replace = Name.replace("Apna","Our")
+// console.log(Name_slice)
+// console.log(Name_index)
+// console.log(Name_replace)
