4e80fda1a94c8b352485ec44e9af0262c57a133b
PranjalGits <pranjalthakre02@gmail.com>
2024-03-28 16:29:57 +0530

    first commit

--- JAVASCRIPT PART 3/Practice.js
+++ JAVASCRIPT PART 3/Practice.js
@@ -10,3 +10,41 @@
 // console.log(Name_slice)
 // console.log(Name_index)
 // console.log(Name_replace)
+
+
+// let arr = ["january","july","march","august"]
+// arr.shift()
+// arr.shift()
+// arr.unshift("june")
+// arr.unshift("july")
+// console.log(arr)
+
+// // splices Method   format-:splice(start,deleteCount,item0......itemN)
+// let colors = ["red", "yellow", "Blue", "Orange", "Pink", "white"]
+// console.log(colors.splice(4))
+// console.log(colors)
+
+// console.log(colors.splice(0,1))
+// console.log(colors)
+
+// console.log(colors.splice(0,1,"black","grey"))
+// console.log(colors)
+
+
+// let months = ['january','july','march','august']
+// console.log(months.splice(0,1))
+// console.log(months)
+// console.log(months.splice(1,0,"june"))
+// console.log(months)
+
+
+// let lang = ['c','c++','html','javascript','python','java','c#','sql']
+// console.log(lang.indexOf('javascript'))
+
+let ttt = [['X',null,"O"],[null,'X',null],['O',null,'X']]
+console.log(ttt)
+ttt[0][1] = "O"
+console.log(ttt)
+
+
+
