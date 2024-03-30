08fe95e15e8575f2946cd56e0db8ef0597b02c85
PranjalGits <pranjalthakre02@gmail.com>
2024-03-30 11:37:58 +0530

    first commit

---
+++ JAVASCRIPT PART 5/JS.js
@@ -0,0 +1,81 @@
+// const post = {
+//   username:"@pranjalthakre",
+//   content:"This is my Web Dev Journey",
+//   likes:150,
+//   reposts:5,
+//   tags:["@promittechools","@DataScientists"]
+// };
+
+// console.log(post)
+// console.log(post["username"])
+// console.log(post.content)
+
+// post.username = "@mohitmalviya";
+// console.log(post)
+// post.gender = "female";
+// console.log(post)
+// delete post.reposts;
+// console.log(post)
+
+
+
+// const classInfo = {
+//   pranjal : {
+//     grade:"A+",
+//     city:"Bhopal"
+//   },
+
+//   sakshi:{
+//     grade:"A",
+//     city:"Vidisha"
+//   }
+// };
+
+// // console.log(classInfo)
+// console.log(classInfo.sakshi)
+// console.log(classInfo.sakshi.city)
+// classInfo.sakshi.city = "Mumbai";
+// classInfo.pranjal.city = "Mumbai";
+// console.log(classInfo)
+
+
+
+// const classInfo = [
+//   {
+//     Name:"Pranjal",
+//     grade:"A+",
+//     city:"Bhopal"
+//   },
+
+//   {
+//     Name:"Sakshi",
+//     grade:"A",
+//     city:"Vidisha"
+//   }
+// ];
+
+// console.log(classInfo)
+
+
+
+// console.log(Math.PI)
+// console.log(Math.E)
+// console.log(Math.abs(-17.5))
+// console.log(Math.pow(5,2))
+// console.log(Math.floor(5.3))
+// console.log(Math.ceil(5.3))
+// console.log(Math.random())
+
+
+
+// let random = Math.floor(Math.random() * 10) + 1;
+// console.log(random)
+
+
+// let random = Math.floor(Math.random() * 100) + 1;
+// console.log(random)
+
+
+let random = Math.floor(Math.random() * 5) + 1;
+console.log(random)
+
