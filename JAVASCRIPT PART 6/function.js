e178db2bf515df902f7cb18626689fee4e12fbc3
PranjalGits <pranjalthakre02@gmail.com>
2024-03-31 18:24:07 +0530

    first commit

--- JAVASCRIPT PART 6/function.js
+++ JAVASCRIPT PART 6/function.js
@@ -53,11 +53,91 @@
 // printTable(2);
 
 
-function sum(a,b) {
-  return a+b;
-}
+// function sum(a,b) {
+//   return a+b;
+// }
 
-let s = sum(3,4);
-console.log(s);
-console.log(sum(5,5))
-console.log(sum(sum(1,1), 3));
+// let s = sum(3,4);
+// console.log(s);
+// console.log(sum(5,5))
+// console.log(sum(sum(1,1), 3));
+
+
+// function getSum(n) {
+//   let sum = 0;
+//   for(let i=1; i<=n; i++) {
+//     sum = sum + i;
+//   }
+//   return sum;
+// }
+
+// console.log(getSum(10))
+
+
+// let str = ["Hii"," ","hello"," ","bye", "!!!!"]
+// function concat(str) {
+//   let result;
+//   for(let i=0; i<str.length; i++) {
+//     result = result + str[i];
+//   }
+//   return result;
+// }
+// console.log(concat(str))
+
+
+// const sum = function(a,b) {
+//   return a+b;
+// }
+// console.log(sum(2,3));
+
+
+
+// function multipleGreet(func, count) {
+//   for(let i=1; i<=count; i++) {
+//     func();
+//   }
+// }
+
+// let greet = function() {
+//   console.log("Hello");
+// }
+
+// multipleGreet(greet, 10);
+
+
+
+// const calculator = {
+//   num:55,
+
+//   add:function(a,b) {
+//     return a+b;
+//   },
+//   sub:function(a,b) {
+//     return a-b;
+//   },
+//   mul:function(a,b) {
+//     return a*b;
+//   }
+// }
+
+// console.log(calculator.num);
+// console.log(calculator.add(1,2));
+// console.log(calculator.sub(6,2));
+// console.log(calculator.mul(6,2));
+
+
+
+// const calculator = {
+//   add(a,b) {
+//     return a+b;
+//   },
+//   sub(a,b) {
+//     return a-b;
+//   },
+//   mul(a,b) {
+//     return a*b;
+//   }
+// }
+// console.log(calculator.add(10,2));
+// console.log(calculator.sub(66,2));
+// console.log(calculator.mul(66,2));
---
+++ JAVASCRIPT PART 6/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+console.log(calculator.num);
+// console.log(calculator.add(1,2));
+// console.log(calculator.sub(6,2));
+// console.log(calculator.mul(6,2));
