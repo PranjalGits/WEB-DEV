463ed25303bc5924743b4cc726ed3d40e6cecee5
PranjalGits <pranjalthakre02@gmail.com>
2024-04-06 09:14:21 +0530

    first commit

---
+++ JAVASCRIPT PART 7/Practice_Qs.js
@@ -0,0 +1,14 @@
+// const sqr = (n) => {
+//   console.log(n*n)
+// }
+// sqr(8)
+
+
+let id = setInterval(() => {
+  console.log("Hello  World!!")
+}, 2000);
+
+setTimeout(() => {
+  clearInterval(id)
+  console.log("Clear interval run")
+}, 12000);
