e1088f3009c241ed168c72c711c73510653540ad
PranjalGits <pranjalthakre02@gmail.com>
2024-04-06 08:36:50 +0530

    first commit

---
+++ JAVASCRIPT PART 7/Miscellaneous.js
@@ -0,0 +1,13 @@
+// Arrow Functions //
+
+// const sum = (a,b) => {
+//   console.log(a+b);
+// }
+// sum(5,5)
+
+
+// Arrow Functions (Implicit Return) //
+// const mul = (a,b) => (
+//    a*b
+// );
+// mul(5,5)
