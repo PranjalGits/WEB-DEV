48ed43d10a49e5ff2fec47db201dc9943eb5a9c3
PranjalGits <pranjalthakre02@gmail.com>
2024-04-06 08:19:31 +0530

    first commit

--- JAVASCRIPT PART 7/Java.js
+++ JAVASCRIPT PART 7/Java.js
@@ -1,13 +1,27 @@
 // this Keyword //
 
-const student = {
-  Name:"Pranjal",
-  age:23,
-  eng:95,
-  math:91,
-  phy:97,
+// const student = {
+//   Name:"Pranjal",
+//   age:23,
+//   eng:95,
+//   math:91,
+//   phy:97,
 
-  getAvg() {
-    let avg = (this.eng + this.math + this.phy / 3);
-  }
-}
+//   getAvg() {
+//     let avg = (this.eng + this.math + this.phy / 3);
+//   }
+// }
+
+
+// try and catch //
+
+// console.log("Hello")
+// console.log("Hello")
+// try {
+//   console.log(a)
+// } catch {
+//   console.log("caugh an error")
+// }
+// console.log('hellooooo')
+// console.log('hellooooo')
+// console.log('hellooooo')
